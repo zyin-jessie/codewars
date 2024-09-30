8eb1f33d36c4039633052dffd4a0e569ff986fcc
Jessie Bartolome <macjessie.bartolome@gmail.com>
2024-09-30 23:53:01 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// find the minimum
+function findSmallestInt(arr) {
+    return Math.min(...arr);
+}
+
+// Example usage:
+console.log(findSmallestInt([34, 15, 88, 2]));       // Output: 2
+console.log(findSmallestInt([34, -345, -1, 100]));    // Output: -345
